dda78b7b64e380b14b8957bb9319a83117687ed0
Abdalrahman-Amin <abdelrhmanmohamed421@gmail.com>
2025-04-05 18:28:38 +0200

    Nested Routes and Index Route

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,16 @@ function App() {
          <BrowserRouter>
             {/* <PageNav /> */}
             <Routes>
-               <Route path="/" element={<Homepage />} />
+               <Route index element={<Homepage />} />
                <Route path="/product" element={<Product />} />
                <Route path="/pricing" element={<Pricing />} />
                <Route path="/login" element={<Login />} />
-               <Route path="/app" element={<AppLayout />} />
+               <Route path="/app" element={<AppLayout />}>
+                  <Route index element={<p>cities route</p>} />
+                  <Route path="cities" element={<p>cities route</p>} />
+                  <Route path="countries" element={<p>countries route</p>} />
+                  <Route path="form" element={<p>form route</p>} />
+               </Route>
                <Route path="*" element={<PageNotFound />} />
             </Routes>
          </BrowserRouter>
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,11 @@
+import { Outlet } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-   return <div className={styles.mapContainer}>Map</div>;
+   return (
+      <div className={styles.mapContainer}>
+         <Outlet />
+      </div>
+   );
 }
 
 export default Map;
