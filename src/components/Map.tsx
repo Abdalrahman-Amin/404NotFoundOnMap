a95b72d6b96747d816469f3e15cb2bfc12505930
Abdalrahman-Amin <abdelrhmanmohamed421@gmail.com>
2025-04-25 19:05:39 +0300

    Programmatically navigation using useNavigation

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,17 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
    const [searchParams, setSearchParams] = useSearchParams();
    const lat = searchParams.get("lat");
    const lng = searchParams.get("lng");
+   const navigate = useNavigate();
+
+   const handleClick = () => {
+      navigate("form");
+   };
 
    return (
-      <div className={styles.mapContainer}>
+      <div className={styles.mapContainer} onClick={handleClick}>
          <h1>Map</h1>
          <h2>
             Position: {lat}, {lng}
